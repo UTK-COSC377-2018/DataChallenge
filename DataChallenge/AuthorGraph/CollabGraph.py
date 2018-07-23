b9343020c51a4f748101856e54dc464cd83076a2
Ian Lumsden <lumsden.ian@gmail.com>
2018-07-22 20:52:39 -0400

    Adds a working version that creates the graph correctly.

--- DataChallenge/AuthorGraph/CollabGraph.py
+++ DataChallenge/AuthorGraph/CollabGraph.py
@@ -10,19 +10,58 @@ class CollabGraph:
             self.graph = Graph(bolt=bolt, secure=secure, host=host, https_port=portNum, user=user, password=pswd)
         else:
             self.graph = Graph(bolt=bolt, secure=secure, host=host, http_port=portNum, user=user, password=pswd)
+        self.graph.run("CREATE CONSTRAINT ON (a:Author) ASSERT a.id IS UNIQUE")
+        nodeList = []
         for fn in fnames:
-            o = open(fn)
-            f = json.load(fn)
-            for i in len(f["authors"]):
+            o = open(fn, "r")
+            f = json.load(o)
+            for i in range(len(f["authors"])-1):
                 auth = Node("Author", name=f["authors"][i]["name"], org=f["authors"][i]["org"])
-                if not self.graph.exists(auth):
-                    self.graph.create(auth)
+                self.graph.merge(auth)
+                if auth not in nodeList:
+                    nodeList.append(auth)
                 for j in range(i+1, len(f["authors"])):
                     auth2 = Node("Author", name=f["authors"][j]["name"], org=f["authors"][j]["org"])
-                    if not self.graph.exists(auth2):
-                        self.graph.create(auth2)
+                    self.graph.merge(auth2)
+                    if auth2 not in nodeList:
+                        nodeList.append(auth2)
                     rel1 = Relationship(auth, "CoAuth", auth2)
                     rel2 = Relationship(auth2, "CoAuth", auth)
-                    if (not self.graph.exists(rel1)) and (not self.graph.exists(rel2)):
-                        self.graph.create(rel1)
-                        self.graph.create(rel2)
+                    self.graph.merge(rel1 | rel2)
+        for idx, node in enumerate(nodeList):
+            node["id"] = idx
+            self.graph.push(node)
+        self.numNodes = len(nodeList)
+
+#    def getAdj(self, nodeIdx):
+
+    def addAuthor(self, name, org):
+        auth = Node("Author", name=name, org=org, id=self.numNodes)
+        self.graph.merge(auth)
+        self.numNodes += 1
+
+    def addAuthor(self, node):
+        assert(isinstance(node, Node))
+        node["id"] = self.numNodes
+        self.graph.merge(node)
+        self.numNodes += 1
+
+    def __add__(self, node):
+        if isinstance(node, Node):
+            addAuthor(node)
+        elif node is list or node is tuple:
+            addAuthor(node[0], node[1])
+        else:
+            raise TypeError("Parameter `node` must be a Py2Neo Node, Python list, or Python tuple.")
+        return
+
+    def __len__(self):
+        return self.numNodes
+
+    def __contains__(self, name):
+        query = "MATCH (a:Author {{name:\"{0:s}\"}}) RETURN a".format(name)
+        return self.graph.run(query).data()
+
+    def __getitem__(self, index):
+        query = "MATCH (a:Author {{id:{0:d} }}) RETURN a".format(index)
+        return self.graph.run(query).data()
